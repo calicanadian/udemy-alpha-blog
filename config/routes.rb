215ef59a4c7a8be555cbafbe279c2f463d7223fe
Your name <Your email>
2019-06-07 15:13:46 -0700

    Unit, integration tests for categories.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/home', to: "pages#home"
   get 'pages/about', to: "pages#about"
   resources :articles
+  resources :categories, except: [:destroy]
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
